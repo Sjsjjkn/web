bc1c9a0a26e6d51bc357a527e4cb8cffc99ccb91
Sjsjjkn <a3406078689@163.com>
2026-05-07 10:19:32 +0800

    chore: 更新项目路径相关的构建缓存文件
    更新了构建过程中生成的缓存文件，主要反映项目路径从"d:\cuddly-octo-tribble"变更为"d:\毕设"

--- api/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ api/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01762170e093cb3c2740c5a15fa67dae3d2063c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f65b05a8f6b2338e9e772fe4cd94e0d4252f11e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
